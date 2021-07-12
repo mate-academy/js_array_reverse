4033fd5c4236c4369c81fee4f0763c8be331f39f
Dmytro_Kuznietsov <kdvgym@gmail.com>
2021-07-12 16:42:31 +0300

    Fixes after sr

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -11,39 +11,32 @@ describe(`Function 'arrayReverse':`, () => {
 
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
+  it(`should return reverse string in array`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
+  it(`should return reverse string in array`, () => {
     expect(arrayReverse(['Hi', 'awesome', 'world'])).toEqual(
       ['dl', 'rowemos', 'ewaiH']);
   });
 
-  it(`should return an empty string
-  if original array consists of an empty string`, () => {
+  it(`should return reverse string in array`, () => {
     expect(arrayReverse(['98', '765'])).toEqual(['56', '789']);
   });
 
-  it(`should return an empty string
-  if original array consists of an empty string`, () => {
+  it(`should return reverse string in array`, () => {
     expect(arrayReverse(['SumMer'])).toEqual(['reMmuS']);
   });
 
-  it(`should return an empty string
-  if original array consists of an empty string`, () => {
+  it(`should return reverse string in array`, () => {
     expect(arrayReverse([''])).toEqual(['']);
   });
 
-  it(`should return an empty string
-  if original array consists of an empty string`, () => {
+  it(`should return reverse string in array`, () => {
     expect(arrayReverse(['hello', ''])).toEqual(['olleh', '']);
   });
 
-  it(`should return an empty string
-  if original array consists of an empty string`, () => {
+  it(`should return reverse string in array`, () => {
     expect(arrayReverse(['h1', 'w0rld', '!@#'])).toEqual(
       ['#@', '!dlr0', 'w1h']);
   });
