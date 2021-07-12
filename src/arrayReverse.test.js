c0d68455e52eb381f3f345671fedc3c67859e93f
Danylo25 <pasichnykdanylo@gmail.com>
2021-07-12 23:01:56 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,33 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Mate'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
+
+  it(`should return an array [4 letters, 7 letters]
+    if original array consists of [4 letters, 7 letters]`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an array [1 letter]
+    if original array consists of [1 letter]`, () => {
+    expect(arrayReverse(['M'])).toEqual(['M']);
+  });
+
+  it(`should return an array [1 digit, 2 digits, 2 digits]
+    if original array consists of [1 digit, 2 digits,
+       2 digits]`, () => {
+    expect(arrayReverse(['1', '23', '45'])).toEqual(['5', '43', '21']);
+  });
+
+  it(`should return an array [1 spec. symb., 4 spec. symb., 3 spec. symb.]
+    if original array consists of [1 digit, 2 digits,
+       2 digits]`, () => {
+    expect(arrayReverse(['!', '@##$', '&*('])).toEqual(['(', '*&$#', '#@!']);
+  });
 });
