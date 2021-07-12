a1cd3f10ccf24cd2f9192e25432153ec8a1fbf19
Dana Oliynyk <dana.victorivna23@gmail.com>
2021-07-12 23:19:25 +0300

    first try

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,44 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Smth'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should return an array with same length as original array`, () => {
+    expect(arrayReverse(['Mate', 'Academy']).length).toBe(2);
+  });
+
+  it(`should return resulting array where first string has
+   same lenght as fisrt string in passed array`, () => {
+    const str1 = 'Mate';
+    const str2 = 'Academy';
+    const resultArray = arrayReverse([str1, str2]);
+
+    expect(resultArray[0].length).toBe(str1.length);
+  });
+
+  it(`should return resulting array where second string has
+   same lenght as seccond string in passed array`, () => {
+    const str1 = 'Mate';
+    const str2 = 'Academy';
+    const resultArray = arrayReverse([str1, str2]);
+
+    expect(resultArray[1].length).toBe(str2.length);
+  });
+
+  it(`should reverse the given strings
+  of the original array consists of 2 value`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should reverse the given strings
+  of the original array consists of different 5 value`, () => {
+    expect(arrayReverse(['I', 'want', 'to', 'go', 'out']))
+      .toEqual(['t', 'uoog', 'ot', 'tn', 'awI']);
+  });
 });
