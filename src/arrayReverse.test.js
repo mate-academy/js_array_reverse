9908362adbc8eb781bcb1ead85da831b2822d705
Andrew <andrew.x.1485@gmail.com>
2021-07-12 15:05:10 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,37 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['omg'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return reversed strings
+    if original array consists of one string`, () => {
+    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  });
+
+  it(`should return reversed strings
+  if original array consists of strings with different length`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should return reversed strings
+    if original array consists of an empty and non empty strings`, () => {
+    expect(arrayReverse(['gmo', '', 'gmo'])).toEqual(['omg', '', 'omg']);
+  });
+
+  it(`should return three empty strings
+    if original array consists of two empty strings by the sides`, () => {
+    expect(arrayReverse(['', 'killme', ''])).toEqual(['', 'emllik', '']);
+  });
+
+  it(`should return an palindrom string
+    if original array consists of palindrom string`, () => {
+    expect(arrayReverse(['omg', 'gmo'])).toEqual(['omg', 'gmo']);
+  });
 });
