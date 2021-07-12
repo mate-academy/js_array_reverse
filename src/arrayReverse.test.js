290f05bc60b5d82076895b0a8578dd4680a3a543
coyotious <coyotious@gmail.com>
2021-07-12 19:08:44 +0300

    added tests

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,37 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
-    if original array consists of an empty string`, () => {
+  if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should return an array with reversed string
+  if original array consists of one string`, () => {
+    expect(arrayReverse(['Mate'])).toEqual(['etaM']);
+  });
+
+  it(`should return an reversed array of strings
+  with first string one character long`, () => {
+    expect(arrayReverse(['o', 'create', 'adaM']))
+      .toEqual(['M', 'adaeta', 'erco']);
+  });
+
+  it(`should return an reversed array of strings
+  ['ymed', 'acAetaM'] for original array ['Mate', 'Academy']`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an reversed array of strings with numbers
+  ['123', '4567890'] for original array ['098', '7654321']`, () => {
+    expect(arrayReverse(['098', '7654321'])).toEqual(['123', '4567890']);
+  });
+
+  it(`should return an reversed array of strings with special symbols
+  ['#$-+', '&!'] for original array ['!&+-', '$#']`, () => {
+    expect(arrayReverse(['!&+-', '$#'])).toEqual(['#$-+', '&!']);
+  });
 });
