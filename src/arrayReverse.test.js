0aa094c1687670735897c2583ed3eb4546cfa9a0
TopsyKretts13 <lypivskiy.artur@gmail.com>
2021-07-12 19:41:14 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,33 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['q1'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should return reversed strings
+    if original array consists of one string`, () => {
+    expect(arrayReverse(['TK13_'])).toEqual(['_31KT']);
+  });
+
+  it(`should return a reverse string elements
+  from original array that contains 2 value`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return reversed strings
+  if original array consists of strings with different length`, () => {
+    expect(arrayReverse(['1', 'qw', '1q@', 'wfdsv24!']))
+      .toEqual(['!', '42', 'vsd', 'fw@q1wq1']);
+  });
+
+  it(`should return a reverse string elements from original
+  array containing spaces`, () => {
+    expect(arrayReverse(['asd1 de', 'sd2 !']))
+      .toEqual(['! 2dsed', ' 1dsa']);
+  });
 });
