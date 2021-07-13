bb33731788a69a84a9dc23a62eb58e1b631e8581
Diana Kyrychuk <85572038+DianaKyrychuk@users.noreply.github.com>
2021-07-13 23:07:32 +0300

    my tests

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,27 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['a2'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([])).toEqual([]);
   });
 
-  // write more tests here
+  it(`should accept letters, numbers and special symbols and reverse`, () => {
+    expect(arrayReverse(['Mate3', 'Academy%'])).toEqual(['%ymed', 'acA3etaM']);
+  });
+
+  it(`should return reverse string if array cconsist 1 string`, () => {
+    expect(arrayReverse(['Day'])).toEqual(['yaD']);
+  });
+
+  it(`should return reverse string if array consist 2+ strings`, () => {
+    expect(arrayReverse(['Day', 'Me', 'Your'])).toEqual(['ruo', 'Ye', 'MyaD']);
+  });
+
+  it(`should return reversed elements with spaces included`, () => {
+    expect(arrayReverse(['Da y'])).toEqual(['y aD']);
+  });
 });
