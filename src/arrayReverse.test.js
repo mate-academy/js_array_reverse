be2631e52aa19296dda33bc56976f5e1d8b70088
Alkayde <morozovnikita5899@gmail.com>
2021-07-13 10:10:56 +0300

    Test

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,25 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['hello'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return an array of strings 
+  staying the same length as in the inputted array but being reversed
+    Example: 
+    ['I', 'am', 'a', 'student!'] --> ['!', 'tn', 'e', 'dutsamaI'] `, () => {
+    // eslint-disable-next-line
+    expect(arrayReverse(['I', 'am', 'a', 'student!'])).toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should return an array of reversed strings
+  regardless of type of data in the strings`, () => {
+    // eslint-disable-next-line
+    expect(arrayReverse(['123', '@!', 'England', 'London'])).toEqual(['nod', 'no', 'Ldnalgn', 'E!@321']);
+  });
 });
