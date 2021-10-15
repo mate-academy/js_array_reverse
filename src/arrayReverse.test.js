2d3d59ebb2c5b0959be41f8e43a1200ab61c02d8
mykolaPakhniuk <pakhniukm@gmail.com>
2021-10-15 13:37:16 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,12 +8,29 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(arrayReverse(['asd'])).toBeInstanceOf(Array);
+  });
 
+  it(`should return an ['ymed', 'acAetaM']
+    if original array consists of ['Mate', 'Academy']`, () => {
+    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
   it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  if original array consists of an empty string`, () => {
+    expect(arrayReverse([' ', ' '])).toEqual([' ', ' ']);
+  });
+
+  it(`should return ['!', 'tn', 'e', 'dutsamaI']
+  if original array consists of ['I', 'am', 'a', 'student!']`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should return array 
+  if original array consists number and special characters`, () => {
+    expect(arrayReverse(['@4', '=3', '+2', '-1']))
+      .toEqual(['1-', '2+', '3=', '4@']);
   });
 
   // write more tests here
