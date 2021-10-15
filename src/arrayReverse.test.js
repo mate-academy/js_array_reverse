7a309bbb523c51f444881231ab4a60e4e429fa52
karinababii <karino4ka201351@gmail.com>
2021-10-15 13:56:48 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,12 +8,26 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['Hello']);
 
+    expect(result).toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
+  it(`should return an empty string 
+  if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should be case sensetive`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  });
+
+  it(`if the length of the first string is 1, 
+  and the length of the last is 8,
+  then when reversing, the first string in the array will contain
+  only the last character from the last string`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
   });
 
   // write more tests here
