c33dfd8caca08ffce205fc4bd6c77743cce9bc65
NikoLazur <lazur.niko@gmail.com>
2021-10-15 17:06:46 +0300

    Init solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,25 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return an array of reversed strings`, () => {
+    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse(['Mate12', 'Academy'])).toEqual(['ymedac', 'A21etaM']);
+    expect(arrayReverse(['Academy'])).toEqual(['ymedacA']);
+  });
+
+  it(`should return empty string for numbers`, () => {
+    expect(arrayReverse([1234, 34567])).toEqual(['', '']);
+  });
+
+  it(`should return original array if array has one character`, () => {
+    expect(arrayReverse(['A'])).toEqual(['A']);
+  });
 });
