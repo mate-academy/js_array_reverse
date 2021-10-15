f0d1a5a84e64bd56ccd6eb968d7ba8d60a82ceec
Oleksandr Kapustian <sasha21041989@gmail.com>
2021-10-15 22:36:31 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,34 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['qwe', 'rty']))
+      .toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should return reversed string and reversed order of string `, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return reversed string`, () => {
+    expect(arrayReverse(['GROBUT!'])).toEqual(['!TUBORG']);
+  });
+
+  it(`should return a reverse array if it contains letters, 
+    numbers and special characters`, () => {
+    expect(arrayReverse(['Number', 'five', '>', 'than', '4']))
+      .toEqual(['4naht>', 'evif', 'r', 'ebmu', 'N']);
+  });
+
+  it(`should return reversed string if it contains numbers`, () => {
+    expect(arrayReverse(['13', '246'])).toEqual(['64', '231']);
+  });
+
+  it(`the length of the elements of the reverse array does not change`, () => {
+    expect(arrayReverse(['123/4', 'abc', '?'])).toEqual(['?cba4', '/32', '1']);
+  });
 });
