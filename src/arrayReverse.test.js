a7203817678dee3d5ecff0c202e4079dca610a9a
Volodymyr Polishchuk <rot010011@gmail.com>
2021-12-13 16:20:31 +0200

    tests are done

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -3,18 +3,15 @@
 describe(`Function 'arrayReverse':`, () => {
   const arrayReverse = require('./arrayReverse');
 
-  it(`should be declared`, () => {
-    expect(arrayReverse).toBeInstanceOf(Function);
-  });
-
-  it(`should return an array`, () => {
-
-  });
-
-  it(`should return an empty string
+  test(`Function should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse(['']))
+      .toEqual(['']);
   });
 
-  // write more tests here
+  test(`Function should return reversed string
+    if original array consists of a valid chars`, () => {
+    expect(arrayReverse(['What', 'c0l0r', '1s', 'a', 'carr0t', '?']))
+      .toEqual(['?t0r', 'racas', '1r', '0', 'l0ctah', 'W']);
+  });
 });
