82cb430832836e3ca44e58d02b3462ac9d0c802d
Corsika1634 <corsika1634@gmail.com>
2022-01-03 18:08:33 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -13,8 +13,31 @@ describe(`Function 'arrayReverse':`, () => {
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should return the empty strings
+    if original array consists of empty strings`, () => {
+    expect(arrayReverse(['', ''])).toEqual(['', '']);
+  });
+
+  it(`should return an empty string first if the //
+      original array contains an empty string as the first element`, () => {
+    expect(arrayReverse(['', 'Mate'])).toEqual(['', 'etaM']);
+  });
+
+  it(`should return '0lleH' if the given string is 'Hell0'`, () => {
+    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  });
+
+  it(`should return ['ymed', 'acAetaM'] if the //
+      given strings is ['Mate', 'Academy']`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return ['!', 'tn', 'e', 'dutsamaI'] if the //
+      given strings is ['I', 'am', 'a', 'student!']`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!'])).toEqual(['!', //
+      'tn', 'e', 'dutsamaI']);
+  });
 });
