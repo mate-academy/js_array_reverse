ab1a7e7d550aed7ceb11adb6a8f898565fe5ac66
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-11-04 14:31:57 +0200

    Tests

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -1,20 +1,68 @@
 'use strict';
 
-describe(`Function 'arrayReverse':`, () => {
-  const arrayReverse = require('./arrayReverse');
+const arrayReverse = require('./arrayReverse');
 
-  it(`should be declared`, () => {
-    expect(arrayReverse).toBeInstanceOf(Function);
+describe(`Function 'arrayReverse' should:`, () => {
+  it(`be declared`, () => {
+    expect(arrayReverse)
+      .toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
-
+  it(`return an array`, () => {
+    expect(arrayReverse(['Mate']))
+      .toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  it(`return array of the same length`, () => {
+    const testArray = ['Mate', 'Academy'];
+    const result = arrayReverse(testArray);
+
+    expect(result)
+      .toHaveLength(2);
   });
 
-  // write more tests here
+  it('not change arrays elements length', function() {
+    const testArray = ['Mate', 'Academy'];
+    const result = arrayReverse(testArray);
+
+    expect(result[0])
+      .toHaveLength(4);
+
+    expect(result[1])
+      .toHaveLength(7);
+  });
+
+  it('work correctly with one word', function() {
+    const testArray = ['Mate'];
+    const result = arrayReverse(testArray);
+
+    expect(result)
+      .toEqual(['etaM']);
+  });
+
+  it(`work correctly with palindrome's`, function() {
+    const testArray = ['rotator', 'rotator'];
+    const result = arrayReverse(testArray);
+
+    expect(result)
+      .toEqual(['rotator', 'rotator']);
+  });
+
+  it('work correctly with words of different length', function() {
+    const testArray = ['I', 'am', 'a', 'student!'];
+    const result = arrayReverse(testArray);
+
+    expect(result)
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it('throw error if no arguments passed', function() {
+    expect(() => arrayReverse())
+      .toThrowError();
+  });
+
+  it('throw error if argument type is not an array', function() {
+    expect(() => arrayReverse(26))
+      .toThrowError();
+  });
 });
