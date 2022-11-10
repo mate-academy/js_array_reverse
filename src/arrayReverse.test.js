2b481a08462ac66c925008c591873eb69c0ceb6d
Marina TORONYI <marina.toronyi>
2022-11-10 21:07:31 +0100

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,34 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Hello'])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([])).toEqual([]);
   });
 
-  // write more tests here
+  test('should return the reversed array ', () => {
+    const world = ['Mate', 'Academy'];
+
+    expect(arrayReverse(world)).toEqual(['ymed', 'acAetaM']);
+  });
+
+  test(`should return the reversed array 
+    when the original array consists of Latin letters, 
+    numbers and special symbols.  `, () => {
+    const world = ['♥M%@', '1&!:'];
+
+    expect(arrayReverse(world)).toEqual([':!&1', '@%M♥']);
+  });
+
+  test(`should return the reversed array and 
+    the length of the strings from the original array 
+    does not change`, () => {
+    const world = ['I', 'am', 'a', 'student!'];
+    const result = arrayReverse(world);
+
+    expect(result).toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
 });
