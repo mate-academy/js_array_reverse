f63b626cf81c95781299529826f718277cd7806a
Andrii Dydyk <andriidydyk622@gmail.com>
2023-01-14 15:27:14 +0100

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -3,18 +3,45 @@
 describe(`Function 'arrayReverse':`, () => {
   const arrayReverse = require('./arrayReverse');
 
-  it(`should be declared`, () => {
+  test(`should be declared`, () => {
     expect(arrayReverse).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
+  test(`should return an array`, () => {
+    const reverseWord = arrayReverse([]);
 
+    expect(reverseWord)
+      .toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
+  test(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    const words = arrayReverse([]);
+
+    expect(words)
+      .toEqual([]);
   });
 
+  test(`should return an reversed string`, () => {
+    const reverseWord = arrayReverse(['Hello']);
+
+    expect(reverseWord)
+      .toEqual(['olleH']);
+  });
+
+  test(`should return reversed string
+   so that the length of the strings from
+   the original array does not change`, () => {
+    const reverseWord = arrayReverse(['Hello', 'friends']);
+
+    expect(reverseWord)
+      .toEqual(['sdnei', 'rfolleH']);
+  });
+
+  test('should throw an error if arguments are not array', () => {
+    expect(() => {
+      arrayReverse();
+    }).toThrow();
+  });
   // write more tests here
 });
