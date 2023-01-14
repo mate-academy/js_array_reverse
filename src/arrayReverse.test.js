4da152158e328d8e6d151ce4f854d30be4077d5c
Andrii Dydyk <andriidydyk622@gmail.com>
2023-01-14 18:11:42 +0100

    Fixed after review

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -16,10 +16,10 @@ describe(`Function 'arrayReverse':`, () => {
 
   test(`should return an empty string
     if original array consists of an empty string`, () => {
-    const words = arrayReverse([]);
+    const words = arrayReverse(['']);
 
     expect(words)
-      .toEqual([]);
+      .toEqual(['']);
   });
 
   test(`should return an reversed string`, () => {
@@ -38,7 +38,57 @@ describe(`Function 'arrayReverse':`, () => {
       .toEqual(['sdnei', 'rfolleH']);
   });
 
-  test('should throw an error if arguments are not array', () => {
+  test(`should return the same string 
+    if the length of the argument equal 1`, () => {
+    const reverseWord = arrayReverse(['b']);
+
+    expect(reverseWord)
+      .toEqual(['b']);
+  });
+
+  test(`should return a reversed string of numbers`, () => {
+    const reverseWord = arrayReverse(['123', '456']);
+
+    expect(reverseWord)
+      .toEqual(['654', '321']);
+  });
+
+  test(`should return a reversed string of numbers
+  so that the length of the strings from
+  the original array does not change`, () => {
+    const reverseWord = arrayReverse(['123', '45']);
+
+    expect(reverseWord)
+      .toEqual(['543', '21']);
+  });
+
+  test(`should return a reversed string of special symbols`, () => {
+    const reverseWord = arrayReverse(['!@#$%']);
+
+    expect(reverseWord)
+      .toEqual(['%$#@!']);
+  });
+
+  test(`should return a reversed string of special symbols
+  so that the length of the strings from
+  the original array does not change`, () => {
+    const reverseWord = arrayReverse(['!@#$', '*&']);
+
+    expect(reverseWord)
+      .toEqual(['&*$#', '@!']);
+  });
+
+  test(`should return a reversed string that
+  consists of Latin letters, numbers and special symbols 
+  so that the length of the strings from
+  the original array does not change`, () => {
+    const reverseWord = arrayReverse(['Hello', '123', '@']);
+
+    expect(reverseWord)
+      .toEqual(['@321o', 'lle', 'H']);
+  });
+
+  test(`should throw an error if arguments are not array`, () => {
     expect(() => {
       arrayReverse();
     }).toThrow();
