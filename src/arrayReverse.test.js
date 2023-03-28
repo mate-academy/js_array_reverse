d93d6799e1fccc54e67145ac84c3bc2ff206d560
Max Nikiforov <maxxnikiforov@gmail.com>
2023-03-28 17:50:34 +0700

    tasts added

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -4,17 +4,38 @@ describe(`Function 'arrayReverse':`, () => {
   const arrayReverse = require('./arrayReverse');
 
   it(`should be declared`, () => {
-    expect(arrayReverse).toBeInstanceOf(Function);
+    expect(arrayReverse)
+      .toBeInstanceOf(Function);
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
 
+    expect(Array.isArray(result))
+      .toBe(true);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse(['']))
+      .toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return array of reversed strings with
+  lenght as in original array`, () => {
+    expect(arrayReverse(['Mate', 'Academy']))
+      .toEqual(['ymed', 'acAetaM']);
+  });
+
+  it(`should return array of reversed strings with
+  lenght as in original array`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should return array of reversed strings with
+  lenght as in original array`, () => {
+    expect(arrayReverse(['---', '++', '????', '!---!']))
+      .toEqual(['!--', '-!', '????', '++---']);
+  });
 });
