3c2d2a773cefac15c28baee6699c94ecdc8b08b4
Dima Cherednyk <dicherednyk@gmail.com>
2023-05-18 14:49:38 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,32 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse([]);
 
+    expect(Array.isArray(result)).toBe(true);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  it(`should return an empty string `
+    + `if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return an array with reverse string`, () => {
+    const result = arrayReverse(['Hell0']);
+
+    expect(result).toEqual(['0lleH']);
+  });
+
+  it(`should return the same length of first `
+    + `string in massive after reverse`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
+
+    expect(result[0].length).toEqual(4);
+  });
+
+  it(`should works with symbols and numbers`, () => {
+    const result = arrayReverse(['I', 'am', 'a', '1student!']);
+
+    expect(result).toEqual(['!', 'tn', 'e', 'duts1amaI']);
+  });
 });
