06cda84edcf7919b1964730618b087a3da3eb216
Vlad <bojko.vladislav16@gmail.com>
2023-06-08 12:36:00 +0300

    All tests have been provided

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,19 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['asdasd'])).toBeInstanceOf(Array)
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it('should reverse the given strings keeping the initial order', () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!'])).toEqual(['!', 'tn', 'e', 'dutsamaI'])
+  });
+
+  it('should not change the initial length', () => {
+    expect(['Mate', 'Academy', 'is', 'the', 'best']).toHaveLength(5)
+  });
 });
