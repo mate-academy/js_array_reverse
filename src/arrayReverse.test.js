518cdabe08dd5e22d448df958a600b7a6c7c52f4
VikkiPetrova <vikkipetrova55@gmail.com>
2023-06-12 14:17:52 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,34 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['ray', 'ray']);
 
+    expect(result).toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  it(`Should return an array equal to the length `
+  + `of the original array`, () => {
+    const words = ['hello', 'world', 'test'];
+    const reversedWords = arrayReverse(words);
+
+    expect(reversedWords.length).toBe(words.length);
   });
 
-  // write more tests here
+  it(`Should return an array with the first line reversed `, () => {
+    const result = arrayReverse(['I', 'am', 'a', 'student!']);
+
+    expect(result).toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`Should return ampty array when imput data []`, () => {
+    const result = arrayReverse([]);
+
+    expect(result).toEqual([]);
+  });
+
+  it(`Should return an array with an inverted string`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
+
+    expect(result).toEqual(['ymed', 'acAetaM']);
+  });
 });
