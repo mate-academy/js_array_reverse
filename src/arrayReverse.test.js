5a4116d24f553926e33d9185693515cf33720326
Viktoriia Tykhomyrova <viktoriavintonak0@gmail.com>
2023-06-23 17:04:32 +0200

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -16,5 +16,35 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an empty string
+   if the original array consists of an empty string`, () => {
+    const result = arrayReverse(['', '']);
+
+    expect(result).toEqual(['', '']);
+  });
+
+  it(`should reverse the strings in
+   the array and maintain the same string length`, () => {
+    const result = arrayReverse(['Hello', 'World', '54321']);
+
+    expect(result).toEqual(['12345', 'dlroW', 'olleH']);
+  });
+
+  it(`should reverse strings with special symbols`, () => {
+    const result = arrayReverse(['@#$%', 'Ab-Cd', '!@#$%^&*()']);
+
+    expect(result).toEqual([')(*&', '^%$#@', '!dC-bA%$#@']);
+  });
+
+  it(`should handle an array with a single string correctly`, () => {
+    const result = arrayReverse(['Hello']);
+
+    expect(result).toEqual(['olleH']);
+  });
+
+  it(`should handle an array with no strings correctly`, () => {
+    const result = arrayReverse([]);
+
+    expect(result).toEqual([]);
+  });
 });
