41438f6d83073c97d80fc1f16b2db5b597e80f12
Dmytro Budynskyi <dbudinskyy@gmail.com>
2023-07-05 15:57:12 +0200

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -16,5 +16,30 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an array equal to the length `
+  + `of the original array`, () => {
+    const words = ['hello', 'world', 'test'];
+    const reversedWords = arrayReverse(words);
+
+    expect(reversedWords.length).toBe(words.length);
+  });
+
+  it(`should return an array with reverse string`, () => {
+    const result = arrayReverse(['Hell0']);
+
+    expect(result).toEqual(['0lleH']);
+  });
+
+  it(`should return the same length of first string`
+  + `in massive after reverse`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
+
+    expect(result[0].length).toEqual(4);
+  });
+
+  it(`should works with symbols and numbers`, () => {
+    const result = arrayReverse(['I', 'am', 'a', '1student!']);
+
+    expect(result).toEqual(['!', 'tn', 'e', 'duts1amaI']);
+  });
 });
