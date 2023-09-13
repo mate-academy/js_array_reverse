3e0f0b2991e950383f7cb0afc7a9efaeac1eb186
Bohdan Svystun <bogdansvystun02@gmail.com>
2023-09-13 14:13:08 +0300

    solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -13,8 +13,30 @@ describe(`Function 'arrayReverse':`, () => {
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`if the length of the first string is 1,
+   and the length of the last is 5,
+    then when reversing, the first string in the
+     array will contain only the last character
+ from the last string.`, () => {
+    expect(arrayReverse(['I', 'koral'])).toEqual(['l', 'arokI']);
+  });
+
+  it(`should return an "'ymed', 'acAetaM'"
+    if original array is "'Mate', 'Academy'"`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an "'0lleH'"
+    if original array is "'Hell0'"`, () => {
+    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  });
+
+  it(`should return an "'!', 'tn', 'e', 'dutsamaI'"
+    if original array is "'I', 'am', 'a', 'student!'"`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
 });
