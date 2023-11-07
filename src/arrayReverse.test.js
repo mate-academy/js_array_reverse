8c9405edd096058568872d760887e7b6574497ac
Mikhail Koponkin <miponkin@gmail.com>
2023-11-07 15:34:58 +0200

    solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,32 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return 'B'
+  if array consist of 'B'`, () => {
+    expect(arrayReverse(['B'])).toEqual(['B']);
+  });
+
+  it(`should return '8145'
+  if array consist of '5418'`, () => {
+    expect(arrayReverse(['5418'])).toEqual(['8145']);
+  });
+
+  it(`should return 'ceD', '321'
+  if array consist of '123', 'Dec'`, () => {
+    expect(arrayReverse(['123', 'Dec'])).toEqual(['ceD', '321']);
+  });
+
+  it(`should return 'ali', 'k', '9@#&'
+  if array consist of '&#@', '9', 'kila'`, () => {
+    expect(arrayReverse(['&#@', '9', 'kila']))
+      .toEqual(['ali', 'k', '9@#&']);
+  });
 });
