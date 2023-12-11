0d69f4c74eaaada374337b4e6435ad7ccb0858a4
sheva-laptop <vladislavshevchenko10@gmail.com>
2023-12-11 19:06:21 +0200

    add tests

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,29 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['Hello']);
 
+    expect(Array.isArray(result)).toBe(true);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    const result = arrayReverse(['']);
+
+    expect(result).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should reverse strings correctly`, () => {
+    const result1 = arrayReverse(['Hell0']);
+
+    expect(result1).toEqual(['0lleH']);
+
+    const result2 = arrayReverse(['Mate', 'Academy']);
+
+    expect(result2).toEqual(['ymed', 'acAetaM']);
+
+    const result3 = arrayReverse(['I', 'am', 'a', 'student!']);
+
+    expect(result3).toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
 });
