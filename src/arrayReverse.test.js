70ee39b232358cc23f5c87921b516859b5c0d0f8
Aleksandr <cebanoleksandr@gmail.com>
2023-12-15 12:53:16 +0200

    solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,30 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
 
+    expect(Array.isArray(result)).toBeTruthy();
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return array with the same length as original array`, () => {
+    const array = ['I', 'am', 'a', 'student!'];
+    const result = arrayReverse(array);
+
+    expect(array.length === result.length).toBeTruthy();
+  });
+
+  it(`should return array with reversed strings`, () => {
+    const array = ['I', 'am', 'a', 'student!'];
+    const copiedArr = ['I', 'am', 'a', 'student!'];
+    const result = arrayReverse(array);
+    const firstStr = copiedArr.join('');
+    const secondStr = result.join('');
+
+    expect(secondStr.split('').reverse().join('')).toBe(firstStr);
+  });
 });
