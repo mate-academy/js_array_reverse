958ca20d557b7543fee7dac2cd054bc9954739f8
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-29 07:32:34 +0200

    Added tests

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -2,13 +2,15 @@
 
 describe(`Function 'arrayReverse':`, () => {
   const arrayReverse = require('./arrayReverse');
+  const wordsExample = ['TS', 'is', 'better', 'then', 'JS', '100%'];
+  const resultExample = arrayReverse(wordsExample);
 
   it(`should be declared`, () => {
     expect(arrayReverse).toBeInstanceOf(Function);
   });
 
   it(`should return an array`, () => {
-
+    expect(typeof resultExample).toEqual('object');
   });
 
   it(`should return an empty string
@@ -16,5 +18,32 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an empty array
+    if original array is empty too`, () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
+
+  it(`should return an reversed array
+    if original array is ['Hell0']`, () => {
+    expect(arrayReverse(['Hell0']))
+      .toEqual(['0lleH']);
+  });
+
+  it(`should return an reversed array
+    if original array is ['Mate', 'Academy']`, () => {
+    expect(arrayReverse(['Mate', 'Academy']))
+      .toEqual(['ymed', 'acAetaM']);
+  });
+
+  it(`should return an reversed array
+    if original array is ['I', 'am', 'a', 'student!']`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!']))
+      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should return an reversed array
+    if original array is ['TS', 'is', 'better', 'then', 'JS', '100%']`, () => {
+    expect(resultExample)
+      .toEqual(['%0', '01', 'SJneht', 'rett', 'eb', 'siST']);
+  });
 });
