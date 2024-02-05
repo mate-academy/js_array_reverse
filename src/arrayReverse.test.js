77cb152dad75e677fd25d7e70684b670f35af7cb
Julia14012000 <Juliamilan1414@gmail.com>
2024-02-05 22:55:22 +0200

    Solution arrayReverse

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,29 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse(['Hello', 'Hey'])).toEqual(['yeHol', 'leH']);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse(['Mate', 'Academy', ''])).toEqual(['ymed', 'acAetaM','']);
   });
 
-  // write more tests here
+  it(`should reverse strings with different lengths`, () => {
+    expect(arrayReverse([' ', 'sun', 'smile', 'funny'])).toEqual(['ynnu', 'fel', 'mssun', ' ']);
+    expect(arrayReverse(['123', '1234567'])).toEqual([  '123','4567123']);
+  });
+
+   it(`should handle an array with a single empty string`, () => {
+    expect(arrayReverse([' ', ' '])).toEqual([' ', ' ']);
+   });
+  
+  it('should handle an empty array', () => {
+    expect(arrayReverse([])).to.deep.equal([]);
+  });
+
+  it('should handle special characters', () => {
+    expect(arrayReverse(['&($', ')^%$#'])).to.deep.equal([')^%', '$#&($']);
+
+  });
 });
