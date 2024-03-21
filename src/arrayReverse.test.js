df1ec732f2e24df2076c83c79033d4e3abb088c3
bla2001 <blasmartlab@gmail.com>
2024-03-21 11:24:03 +0200

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,32 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['Hell0']);
 
+    expect(Array.isArray(result)).toBe(true);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  it(`should reverse a single string`, () => {
+    const result = arrayReverse(['Hell0']);
+
+    expect(result).toEqual(['0lleH']);
   });
 
-  // write more tests here
+  it(`should reverse strings with the original length`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
+
+    expect(result).toEqual(['ymed', 'acAetaM']);
+  });
+
+  it(`should return an empty array if input array is empty`, () => {
+    const result = arrayReverse([]);
+
+    expect(result).toEqual([]);
+  });
+
+  it(`should reverse a single empty string correctly`, () => {
+    const result = arrayReverse(['']);
+
+    expect(result).toEqual(['']);
+  });
 });
