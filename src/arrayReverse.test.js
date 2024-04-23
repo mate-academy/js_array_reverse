468d079e45417e5b73545280942765724f95be73
Rozdorozhnii <rozdorozhnii@ukr.net>
2024-04-23 17:58:54 +0300

    Solution_v1.0

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,32 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['']);
 
+    expect(result).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
-  // write more tests here
+  it(`should return array with same length`, () => {
+    const result = arrayReverse(['Mate', 'Academy']);
+
+    expect(result.length).toBe(2);
+  });
+
+  it(`should return array with same elements length`, () => {
+    const result = arrayReverse(['I', 'am', 'a', 'student!']);
+    const expectedResult = ['!', 'tn', 'e', 'dutsamaI'];
+
+    expect(result[0]).toBe(expectedResult[0]);
+    expect(result[result.length - 1]).toBe(expectedResult[result.length - 1]);
+  });
+
+  it(`should return the same length array with same`
+   + `length of elements with reversed 'chars'`, () => {
+    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  });
 });
