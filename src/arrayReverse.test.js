04163ac211cce6037fcff65dd7f6c6d03bdd1ed0
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-07-08 21:34:08 +0300

    solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,7 +8,22 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    expect(arrayReverse(['Hello'])).toBeInstanceOf(Array);
+  });
 
+  it('return ["!dlro", "wolle", "H"] if'
+    + 'input = ["Hello", "world", "!"]', () => {
+    const params = ['Hello', 'world', '!'];
+    const response = ['!dlro', 'wolle', 'H'];
+
+    expect(arrayReverse(params)).toEqual(response);
+  });
+
+  it('should work with symbols and numbers', () => {
+    const params = ['1234', '!@#$', '09)(', '87*&'];
+    const response = ['&*78', '()90', '$#@!', '4321'];
+
+    expect(arrayReverse(params)).toEqual(response);
   });
 
   it(`should return an empty string
@@ -16,5 +31,37 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it('should return empty array', () => {
+    expect(arrayReverse([''])).toEqual([ '' ]);
+  });
+
+  it('should work correctly with very long string', () => {
+    const params = [
+      'Messi',
+      'is',
+      'the',
+      'best',
+      'football',
+      'player',
+      'in',
+      'the',
+      'world',
+      'ever',
+    ];
+
+    const response = [
+      'reved',
+      'lr',
+      'owe',
+      'htni',
+      'reyalpll',
+      'abtoof',
+      'ts',
+      'ebe',
+      'htsii',
+      'sseM',
+    ];
+
+    expect(arrayReverse(params)).toEqual(response);
+  });
 });
