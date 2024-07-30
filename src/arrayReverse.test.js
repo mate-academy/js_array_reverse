fa1c6cad1bfd98425a647b654dec2a2e4d2c12aa
Kate Hubko <gubkoekaterina9592@gmail.com>
2024-07-30 17:02:23 +0300

    ArrayReverse

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -1,20 +1,46 @@
 'use strict';
 
-describe(`Function 'arrayReverse':`, () => {
-  const arrayReverse = require('./arrayReverse');
+const arrayReverse = require('./arrayReverse');
 
-  it(`should be declared`, () => {
-    expect(arrayReverse).toBeInstanceOf(Function);
-  });
+it('should be declared', () => {
+  expect(arrayReverse).toBeInstanceOf(Function);
+});
 
-  it(`should return an array`, () => {
+it('should return an array', () => {
+  expect(arrayReverse([])).toEqual([]);
+});
 
-  });
+it('should handle empty strings', () => {
+  expect(arrayReverse(['', '', ''])).toEqual(['', '', '']);
+});
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
-  });
+it('should reverse a single word', () => {
+  expect(arrayReverse(['Python'])).toEqual(['nohtyP']);
+});
 
-  // write more tests here
+it('should reverse words in 2-item array', () => {
+  expect(arrayReverse(['Go', 'Future'])).toEqual(['er', 'utuFoG']);
+});
+
+it('should reverse words in 3-item array', () => {
+  expect(arrayReverse(['Data', 'Science', 'Rocks']))
+    .toEqual(['skco', 'Recneic', 'SataD']);
+});
+
+it('should reverse words in 4-item array', () => {
+  expect(arrayReverse(['This', 'is', 'a', 'Dog']))
+    .toEqual(['goDa', 'si', 's', 'ihT']);
+});
+
+it('should reverse words in 5-item array', () => {
+  expect(arrayReverse(['She', 'is', 'more', 'beautiful', 'then', 'Tamara']))
+    .toEqual(['ara', 'ma', 'Tneh', 'tlufituae', 'bero', 'msiehS']);
+});
+
+it('should reverse numbers in 2-item array', () => {
+  expect(arrayReverse(['963', '2587'])).toEqual(['785', '2369']);
+});
+
+it('should reverse symbols in 2-item array', () => {
+  expect(arrayReverse(['@#', '%$*^'])).toEqual(['^*', '$%#@']);
 });
