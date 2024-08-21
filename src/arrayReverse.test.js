12871c3d4eed042accf959843d2989ca39ada000
Bozhenko Roman <bozhenko091999@gmail.com>
2024-08-21 21:34:05 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,62 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([''])).toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  it('should return an empty string '
+    + 'if original array consists of an empty string', () => {
+    expect(arrayReverse(['', '']))
+      .toEqual(['', '']);
   });
 
-  // write more tests here
+  it('should correctly work with long strings', () => {
+    const StringArray = [
+      'a',
+      'bC',
+      'dEf',
+      'gHiJ',
+      'kLmNo',
+      'pQrStU',
+      'vWxYz',
+      'aBcD',
+      'eFg',
+      'hI',
+    ];
+
+    const ExpectedResult = [
+      'I',
+      'hg',
+      'FeD',
+      'cBaz',
+      'YxWvU',
+      'tSrQpo',
+      'NmLkJ',
+      'iHgf',
+      'EdC',
+      'ba',
+    ];
+
+    expect(arrayReverse(StringArray))
+      .toEqual(ExpectedResult);
+  });
+
+  it('should ignore empty strings', () => {
+    const StringArray = [
+      '',
+      'Abc',
+      'De',
+      '',
+    ];
+
+    const ExpectedResult = [
+      '',
+      'eDc',
+      'bA',
+      '',
+    ];
+
+    expect(arrayReverse(StringArray))
+      .toEqual(ExpectedResult);
+  });
 });
