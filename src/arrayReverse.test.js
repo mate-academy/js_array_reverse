e9d20c3015e39da37e0f39285ba01e9385d7c4c6
Oleg <osandzlavi@gmail.com>
2024-09-09 21:42:35 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -16,5 +16,40 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an empty array when the input array is empty`, () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
+
+  it(`should correctly reverse a string of length 1`, () => {
+    expect(arrayReverse(['A'])).toEqual(['A']);
+  });
+
+  it(`should correctly reverse a string of length 2`, () => {
+    expect(arrayReverse(['AB'])).toEqual(['BA']);
+  });
+
+  it(`should correctly reverse a string of length 4`, () => {
+    expect(arrayReverse(['Test'])).toEqual(['tseT']);
+  });
+
+  it(`should correctly reverse a string of length 5`, () => {
+    expect(arrayReverse(['Hello'])).toEqual(['olleH']);
+  });
+
+  it(`should correctly reverse a string of length 6`, () => {
+    expect(arrayReverse(['Mateys'])).toEqual(['syetaM']);
+  });
+
+  it(`should handle an array with strings containing exactly
+  one special character and one number in each string`, () => {
+    expect(arrayReverse(['1a!', '2b@', '3c#'])).toEqual(['#c3', '@b2', '!a1']);
+  });
+
+  it(`should correctly reverse an array with single-character strings`, () => {
+    expect(arrayReverse(['A', 'B', 'C'])).toEqual(['C', 'B', 'A']);
+  });
+
+  it(`should handle an array with identical strings`, () => {
+    expect(arrayReverse(['abc', 'abc', 'abc'])).toEqual(['cba', 'cba', 'cba']);
+  });
 });
