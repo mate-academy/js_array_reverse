4c13bb0d3ed4e0371ecf54bcf7111cff476459a7
Yevhen Baiev <baev838@gmail.com>
2024-09-12 17:48:28 +0300

    Tests for arrayReverse function are prepeared

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -4,17 +4,41 @@ describe(`Function 'arrayReverse':`, () => {
   const arrayReverse = require('./arrayReverse');
 
   it(`should be declared`, () => {
-    expect(arrayReverse).toBeInstanceOf(Function);
+    expect(arrayReverse)
+      .toBeInstanceOf(Function);
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([]))
+      .toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse(['']))
+      .toEqual(['']);
   });
 
-  // write more tests here
+  it('should return an empty array if empty array was passed', () => {
+    expect(arrayReverse([]))
+      .toEqual([]);
+  });
+
+  it('should return reversed string if original'
+    + 'array consists of string with numbers', () => {
+    expect(arrayReverse(['2D2R']))
+      .toEqual(['R2D2']);
+  });
+
+  it('should return reversed string if original'
+    + 'array consists of string with special symbols', () => {
+    expect(arrayReverse(['№# - dff']))
+      .toEqual(['ffd - #№']);
+  });
+
+  it('should return reversed strings in array '
+    + 'if array with multiple strings was passed', () => {
+    expect(arrayReverse(['Holly', 'guacamole', 'you', 'did', 'it!']))
+      .toEqual(['!tidi', 'duoyeloma', 'cau', 'gyl', 'loH']);
+  });
 });
