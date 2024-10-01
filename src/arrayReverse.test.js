1bb86aa5cd125bcc4f750e36f865fb5abd714682
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-10-01 14:30:18 +0200

    add task solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -13,24 +13,32 @@ describe(`Function 'arrayReverse':`, () => {
     expect(result).toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['', 'Mate', '', 'Academy']))
-      .toEqual(['', 'ymed', '', 'acAetaM']);
+  it(`should return an empty array if the original array is empty`, () => {
+    expect(arrayReverse([])).toEqual([]);
   });
 
-  it(`should return an inverted array`, () => {
-    expect(arrayReverse(['Hell0'])).toEqual(['0lleH']);
+  it(`should return an inverted array consisting of numbers`, () => {
+    expect(arrayReverse([123, 456])).toEqual([321, 654]);
   });
 
-  it(`should return an inverted array preserving the length`
-    + `and order of the elements of the original array`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  // eslint-disable-next-line max-len
+  it(`should return an array where each element with special characters is reversed`, () => {
+    expect(arrayReverse(['hello@', 'world#'])).toEqual(['@olleh', '#dlrow']);
   });
 
-  it(`should return an inverted array preserving the length and order of
-    elements including special characters from the original array`, () => {
-    expect(arrayReverse(['I', 'am', 'a', 'student!']))
-      .toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  it(`should return an inverted string array with varying lengths`, () => {
+    // eslint-disable-next-line max-len
+    expect(arrayReverse(['a', 'bb', 'ccc', 'dddd'])).toEqual(['a', 'bb', 'ccc', 'dddd']);
+  });
+
+  // eslint-disable-next-line max-len
+  it(`should return an inverted array preserving the length and order of elements including special characters`, () => {
+    // eslint-disable-next-line max-len
+    expect(arrayReverse(['I', 'am', 'a', 'student!'])).toEqual(['!', 'tn', 'e', 'dutsamaI']);
+  });
+
+  it(`should return an inverted array while handling mixed data types`, () => {
+    // eslint-disable-next-line max-len, no-undef
+    expect(arrayReverse(['text', 123, null, true])).toEqual(['txet', 321, null, eurt]);
   });
 });
