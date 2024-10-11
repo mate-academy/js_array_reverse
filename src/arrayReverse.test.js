71305d0d24d7c29d1b0b8461ba4b6a9bb5174312
Maksim <teseygrecia@gmail.com>
2024-10-11 14:56:32 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,13 +8,39 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
+    const result = arrayReverse(['test']);
 
+    expect(Array.isArray(result)).toBe(true);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  it(`should return an empty array when the input array is empty`, () => {
+    expect(arrayReverse([])).toEqual([]);
   });
 
-  // write more tests here
+  it(`should return an array with empty strings`, () => {
+    expect(arrayReverse(['', ''])).toEqual(['', '']);
+  });
+
+  it(`should reverse a single word`, () => {
+    expect(arrayReverse(['Hello'])).toEqual(['olleH']);
+  });
+
+  it(`should handle an array with special characters`, () => {
+    expect(arrayReverse(['@test!', '#example$', '%special^']))
+      .toEqual(['^laice', 'ps%$elpma', 'xe#!tset@']);
+  });
+
+  it(`should handle an array with 1 special symbol`, () => {
+    expect(arrayReverse(['string!', '@123', 'hi#']))
+      .toEqual(['#ih321@', '!gni', 'rts']);
+  });
+
+  it(`should return an array with reversed strings of varying lengths`, () => {
+    expect(arrayReverse(['short', 'longer', 'longest']))
+      .toEqual(['tsegn', 'olregn', 'oltrohs']);
+  });
+
+  it(`should handle an array with identical strings`, () => {
+    expect(arrayReverse(['abc', 'abc', 'abc'])).toEqual(['cba', 'cba', 'cba']);
+  });
 });
