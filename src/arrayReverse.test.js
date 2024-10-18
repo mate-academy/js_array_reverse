42b3219b5d5e77fee6f6a86c42835be22da780f7
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-10-18 22:44:30 +0200

    add task solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -11,8 +11,8 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse([])).toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
+  it(`should return an empty string if original array consists of
+    an empty string`, () => {
     expect(arrayReverse([''])).toEqual(['']);
   });
 
@@ -20,25 +20,34 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Bugy'])).toEqual(['yguB']);
   });
 
-  it(`should return the reverse array from the input array which
-  consist from two different strings`, () => {
+  it(`should return the reverse array from the input array which consists
+    of two different strings`, () => {
     expect(arrayReverse(['Bugy', 'cockroach'])).toEqual(['hcao', 'rkcocyguB']);
   });
 
-  it(`should return the reverse array from the input array which
-  consist from three different strings`, () => {
+  it(`should return the reverse array from the input array which consists
+    of three different strings`, () => {
     expect(arrayReverse(['Bugy', 'cockroach', 'worms']))
       .toEqual(['smro', 'whcaorkco', 'cyguB']);
   });
 
-  it(`should return the reverse array from the input array which
-  consist from three different strings with different length`, () => {
+  it(`should return the reverse array from the input array which consists
+    of three different strings with different length`, () => {
     expect(arrayReverse(['Bugy', 'is', 'a', 'cockroach']))
       .toEqual(['hcao', 'rk', 'c', 'ocasiyguB']);
   });
 
-  it(`should return the reverse array from the input array which
-  consist from string where are special char and num`, () => {
-    expect(arrayReverse(['p/d ? - 33,2%3!'])).toEqual(['!3%2,33 - ? d/p']);
+  it(`should return the reverse array from the input array which consists
+    of strings with special chars and numbers`, () => {
+    expect(arrayReverse(['p/d ? - 33,2%3!']))
+      .toEqual(['!3%2,33 - ? d/p']);
+  });
+
+  it(`should return an empty array if input is an empty array`, () => {
+    expect(arrayReverse([])).toEqual([]);
+  });
+
+  it(`should return an array with numbers converted to empty strings`, () => {
+    expect(arrayReverse([123, 456])).toEqual(['', '']);
   });
 });
