1a63944547e4ebfa67b6acfd4e5ebcdc89f5e400
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-23 13:42:14 +0300

    solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -16,5 +16,26 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
-  // write more tests here
+  it(`should return an array with same length
+  as original array length`, () => {
+    const arrLength = arrayReverse(['Mate', 'Academy']);
+
+    expect(arrLength.length).toBe(2);
+  });
+
+  it(`should not change length of strings in array after reverse`, () => {
+    const arrLength = arrayReverse(['Mate', 'Academy']);
+
+    expect(arrLength[0].length).toBe(4);
+  });
+
+  it(`should return empty array if empty array used as an argument`, () => {
+    const arrLength = arrayReverse([]);
+
+    expect(arrLength).toEqual([]);
+  });
+
+  it(`should not change length of strings in array after reverse`, () => {
+    expect(() => arrayReverse()).toThrow();
+  });
 });
