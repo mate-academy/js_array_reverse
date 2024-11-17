79ac80a2313a101a21384fdb3738187d00bfc04c
VVladLytvyn <1997vlad1997bob@gmail.com>
2024-11-17 16:30:45 +0200

    solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -7,14 +7,29 @@ describe(`Function 'arrayReverse':`, () => {
     expect(arrayReverse).toBeInstanceOf(Function);
   });
 
-  it(`should return an array`, () => {
-
+  it(`should return an array of reversed worls`, () => {
+    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([])).toEqual([]);
   });
 
-  // write more tests here
+  it(`should return an array of reversed worls including special symbols`, () => {
+    expect(arrayReverse(['&Mate', 'Academy'])).toEqual(['ymeda', 'cAetaM&']);
+  });
+
+  it(`should return an array of reversed worls including special numbers`, () => {
+    expect(arrayReverse(['8Mate', 'Academy'])).toEqual(['ymeda', 'cAetaM8']);
+  });
+
+  it(`the letngth of the strings should be the same as in original array`, () => {
+    const arr = ['8Mate', 'Academy'];
+
+    arrayReverse(arr);
+
+    expect(arr[0].length).toBe(5);
+    expect(arr[1].length).toBe(7);
+  });
 });
